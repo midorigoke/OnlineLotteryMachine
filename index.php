4f46ad548225f47ff27826acfcb4ee802bce4a98
midorigoke <midorigoke@desumachi.tk>
2017-03-26 21:53:29 +0900

    add: slave side

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>オンライン抽選機</title>
+		<script src="./js/chaffle.min.js"></script>
+	</head>
+	<body>
+		<h1>オンライン抽選機</h1>
+		<h2>抽選結果</h2>
+		<p id="result"></p>
+		<script src="./js/get_result.js"></script>
+	</body>
+</html>
---
+++ js/get_result.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const result_element = document.getElementById('result');
+
+let result_old;
+
+const result_xhr = new XMLHttpRequest();
+
+function checkUpdate(){
+	result_xhr.onreadystatechange = function(){
+		if (result_xhr.readyState === 4 && result_xhr.status === 200){
+			if (result_xhr.responseText !== result_old){
+				const list_xhr = new XMLHttpRequest();
+
+				list_xhr.onreadystatechange = function(){
+					if (list_xhr.readyState === 4 && list_xhr.status === 200){
+						const list_array = list_xhr.responseText.split('\n');
+
+						let shuffle_text_array = [];
+
+						while (shuffle_text_array.length < 50){
+							shuffle_text_array = shuffle_text_array.concat(list_array);
+						}
+
+						shuffle_text_array = shuffle_text_array.slice(0, 50);
+
+						shuffle_text_array.push(result_xhr.responseText);
+
+						let i = 0;
+
+						const shuffle_job = window.setInterval(function(){
+							result_element.textContent = shuffle_text_array[i];
+
+							i++
+
+							if (i > 50){
+								window.clearInterval(shuffle_job);
+							}
+						}, 25);
+					}
+					
+					result_old = result_xhr.responseText;
+				}
+
+				list_xhr.open('GET', './list.txt');
+
+				list_xhr.send();
+			}
+		}	
+	}
+
+	result_xhr.open('GET', './result.txt');
+
+	result_xhr.send();
+}
+
+checkUpdate();
+
+window.setInterval(checkUpdate, 1000);
+
